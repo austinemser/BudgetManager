af42ff1bb2c72e1db6985e970cdf461de7b4202b
Austin Emser <austinemser@Austins-MacBook-Pro.local>
2013-07-23 20:34:24 -0400

    Initial Commit

---
+++ BudgetManager/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BudgetManager
+//
+//  Created by Austin Emser on 7/23/13.
+//  Copyright (c) 2013 Austin Emser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ BudgetManager/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BudgetManager
+//
+//  Created by Austin Emser on 7/23/13.
+//  Copyright (c) 2013 Austin Emser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
